784c0da987f957949838ff37344a023ff215c64e
Facundo M <105024960+Facu6M@users.noreply.github.com>
2022-06-05 19:32:21 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+const email = document.getElementById('email')
+
+email.addEventListener("click", event  => {
+ alert("my email is: facu6p@gmail.com")
+})
+
+
+const phone = document.getElementById('phone')
+
+phone.addEventListener("click", event  => {
+ alert("my phone number is : 11-5145-1631")
+})
